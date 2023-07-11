849c784079fe3eb94a46bbcf78d90df20ff1490c
DlabrecquePE <76122172+DlabrecquePE@users.noreply.github.com>
2023-07-10 22:21:17 -0400

    Update Pi-by-Monte-Carlo.py

--- Pi-by-Monte-Carlo.py
+++ Pi-by-Monte-Carlo.py
@@ -1,48 +1,50 @@
-#Libraries
+# libraries
 import matplotlib.pyplot as plt
 import numpy as np
-# %matplotlib
 
-#initialize
-actual=3.141592653589793238462643383279
-batchsize = 255
+# initialize
+actual = 3.141592653589793238462643383279
+batch_size = 100
 
 inside = 0
 outside = 0
 plot_sample = 0
 
-plt.figure(figsize=(8,8))
-plt.axis([-1.1,1.1,-1.1,1.1])
+plt.ion()
+plt.figure(figsize=(8, 8))
+plt.axis([-1.1, 1.1, -1.1, 1.1])
 plt.title("Pi by Monte Carlo Method")
 
 while True:
+    # generate data
+    sample = np.array([np.random.uniform(-1, 1, size=batch_size), np.random.uniform(-1, 1, size=batch_size)])
+    inner_x, inner_y, outer_x, outer_y = ([] for _ in range(4))
 
-    #generate data
-    sample=np.array([np.random.uniform(-1,1, size = batchsize),np.random.uniform(-1,1, size = batchsize)])
-    magnitudes=[]
-    for elem in range(batchsize):
-        magnitudes.append(np.sqrt((sample[0,elem]**2)+(sample[1,elem]**2)))
-
-    #calculate and plot
-    for x in range(batchsize):
-        if magnitudes[x] > 1:
-            outside+=1
-            plt.scatter(sample[0,x],sample[1,x],color='blue')
+    # calculate and plot
+    for point in range(batch_size):
+        if np.sqrt(sample[0, point] ** 2 + sample[1, point] ** 2) > 1:
+            outside += 1
+            outer_x.append(sample[0, point])
+            outer_y.append(sample[1, point])
         else:
-            inside+=1
-            plt.scatter(sample[0,x],sample[1,x],color='red')
-        s=4*(inside)/(inside+outside)
-        d=np.abs(s-actual)
+            inside += 1
+            inner_x.append(sample[0, point])
+            inner_y.append(sample[1, point])
+    pi_approx = 4 * inside / (inside + outside)
+    pi_error = np.abs(pi_approx - actual)
+    plt.scatter(outer_x, outer_y, color='blue')
+    plt.scatter(inner_x, inner_y, color='red')
 
-    #manage output
-    plot_sample+=batchsize
-    output_string1='Pi is approximately '+str(s)
-    output_string2='Off from the actual value by '+str(d)
-    output_string3='Sample # '+str(plot_sample)
-    out1=plt.text(-0.6,-1.25,output_string1)
-    out2=plt.text(-0.6,-1.30,output_string2)
-    out3=plt.text(-0.6,-1.35,output_string3)
-    plt.pause(1)
+    # manage output
+    plot_sample += batch_size
+    output_string1 = 'Pi is approximately ' + str(pi_approx)
+    output_string2 = 'Off from the actual value by ' + str(pi_error)
+    output_string3 = 'Sample # ' + str(plot_sample)
+    out1 = plt.text(-0.6, -1.25, output_string1)
+    out2 = plt.text(-0.6, -1.30, output_string2)
+    out3 = plt.text(-0.6, -1.35, output_string3)
+    plt.pause(.001)
     out1.remove()
     out2.remove()
     out3.remove()
+    
