151f966728b204ab40797f1e9ff4f3b7ab7ebcb8
DlabrecquePE <76122172+DlabrecquePE@users.noreply.github.com>
2023-07-11 00:40:40 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -10,7 +10,7 @@ batch_size, inside, outside, plot_sample = 250, 0, 0, 0
 fig, ax = plt.subplots()
 fig.ion()
 ax.axis([-1.1, 1.1, -1.1, 1.1])
-ax.set_title("Pi by Monte Carlo Method")
+fig.set_title("Pi by Monte Carlo Method")
 
 while True:
     # generate data
