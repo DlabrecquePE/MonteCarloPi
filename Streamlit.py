84f4aec7241eb2fd9a8054ad1475aaba5d0bb87b
DlabrecquePE <76122172+DlabrecquePE@users.noreply.github.com>
2023-07-11 00:49:18 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -8,7 +8,7 @@ actual = 3.141592653589793238462643383279
 batch_size, inside, outside, plot_sample = 250, 0, 0, 0
 
 fig, ax = plt.subplots()
-fig.ion()
+# fig.ion()
 ax.axis([-1.1, 1.1, -1.1, 1.1])
 # ax.set_title('Pi by Monte Carlo Method')
 
